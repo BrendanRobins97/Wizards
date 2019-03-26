931d9411e60e499e0accb058f38a24850200022a
BrendanRobins97 <brendr1@umbc.edu>
2019-03-26 19:39:10 -0400

    Revert "Multiplayer Updates"
    This reverts commit 397a20466b9e1f755976c3530c18d69213b1665a.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -69,8 +69,6 @@ public class Player : MonoBehaviour {
         drsc = FindObjectOfType<DeathRainSpellCamera>();
         special = false;
         originalFOV = playerCamera.fieldOfView;
-        Enable();
-        
     }
 
     private void Update() {
@@ -131,7 +129,7 @@ public class Player : MonoBehaviour {
 
         currentSpellIndex = Mathf.Clamp(currentSpellIndex, 0, spells.Count - 1);
 
-        GameManager.instance?.UpdateSpellImage(currentSpellIndex);
+        GameManager.instance.UpdateSpellImage(currentSpellIndex);
 
         if (Input.GetButtonUp("Fire1")) {
             if (currentSpellIndex == 3 && !special)
