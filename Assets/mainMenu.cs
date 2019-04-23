3f15a709e9349246434285f3621d3bd30853598f
Steven Heckman <heckman1@ad.umbc.edu>
2019-04-23 19:21:43 -0400

    added pointer for player amount in main menu

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -12,10 +12,13 @@ public class mainMenu : MonoBehaviour
     bool teams;
     public static int numPlayers;
     float timer;
+    float timer2;
     private bool startGame = true;
     public static mainMenu instance = null;
     public AudioSource audioData;
     public AudioClip mainTheme;
+    int hoverNumPlayers;
+    public GameObject arrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +29,7 @@ public class mainMenu : MonoBehaviour
         myAnimator = myCamera.GetComponent<Animator>();
         //myAnimator.Play("p1");
         myAnimator.Play("part1");
+        hoverNumPlayers = 4;
     }
 
     void Awake()
@@ -52,9 +56,29 @@ public class mainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(state == 3 || state == 5)
+        {
+            if (hoverNumPlayers == 1) { arrow.transform.position = new Vector3(-3.01f, 1.37f, 4.594f); }
+            if (hoverNumPlayers == 2) { arrow.transform.position = new Vector3(-3.429f, 1.38f, 4.48f); }
+            if (hoverNumPlayers == 3) { arrow.transform.position = new Vector3(-3.925f, 1.392f, 4.344f); }
+            if (hoverNumPlayers == 4) { arrow.transform.position = new Vector3(-4.48f, 1.405f, 4.193f); }
+            if (hoverNumPlayers == 5) { arrow.transform.position = new Vector3(-5.178f, 1.421f, 4.003f); }
+            if (hoverNumPlayers == 6) { arrow.transform.position = new Vector3(-5.696f, 1.433f, 3.862f); }
+            if (hoverNumPlayers == 7) { arrow.transform.position = new Vector3(-6.34f, 1.448f, 3.686f); }
+            if (hoverNumPlayers == 8) { arrow.transform.position = new Vector3(-6.881f, 1.461f, 3.539f); }
+        }
         if (state == 4)
         {
             timer += Time.deltaTime;
+        }
+        if (state == 5)
+        {
+            timer2 += Time.deltaTime;
+            if(timer2 >= .25)
+            {
+                state = 3;
+                timer2 = 0.0f;
+            }
         }
         if(timer >= 2.6f && startGame)
         {
@@ -127,6 +151,33 @@ public class mainMenu : MonoBehaviour
                 numPlayers = 8;
                 myAnimator.Play("part4");
                 state = 4;
+            }
+            else if (state == 3 && Input.GetAxisRaw("Submit") > 0.0)
+            {
+                numPlayers = hoverNumPlayers;
+                myAnimator.Play("part4");
+                state = 4;
+            }
+            else if (state == 3)
+            {
+                if (Input.GetAxis("Horizontal") < -0.2)
+                {
+                    if (hoverNumPlayers > 1)
+                    {
+                        hoverNumPlayers -= 1;
+                        print(timer2 % .5);
+                        state = 5;
+                    }
+                }
+                else if (Input.GetAxis("Horizontal") > 0.2)
+                {
+                    if (hoverNumPlayers < 8)
+                    {
+                        hoverNumPlayers += 1;
+                        print("g");
+                        state = 5;
+                    }
+                }
             }
         }
     }
