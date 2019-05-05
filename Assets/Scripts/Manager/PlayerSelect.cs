d7cb3a5ef6dffa774aacd796eaf37d3cc24c96c8
Micah DeBruno <32474022+micahdebruno@users.noreply.github.com>
2019-05-05 07:59:42 -0400

    Frog melee/PlayerSelect

--- Assets/Scripts/Manager/PlayerSelect.cs
+++ Assets/Scripts/Manager/PlayerSelect.cs
@@ -75,32 +75,38 @@ public class PlayerSelect : MonoBehaviour
                 showPickedTime = 3f;
             }
         }
+
         if (showTutorial)
         {
             canvas.enabled = false;
             tutorialCanvas.enabled = true;
         }
+
         if (gameStartTimer > 3 && gameStartTimer < 6 && !showTutorial)
         {
-            text.text = "Player " + (playerDisplay-1) + " You Chose " + players[currentIndex].name;
+            text.text = "Player " + (playerDisplay - 1) + " You Chose " + players[currentIndex].name;
             //used.Add(currentIndex);
         }
-        if(gameStartTimer <= 3 && gameStartTimer > 0 && !showTutorial)
+
+        if (gameStartTimer <= 3 && gameStartTimer > 0 && !showTutorial)
         {
             text.text = "Lets Play!";
             gameStartTimer = 15f;
             showTutorial = true;
         }
-        if (gameStartTimer <= 0 && gameStartTimer >= -2 || Input.GetButtonUp("Start")||Input.GetKeyDown(KeyCode.S))
+
+        if (gameStartTimer <= 0 && gameStartTimer >= -2 || Input.GetButtonUp("Start") || Input.GetKeyDown(KeyCode.S))
         {
             Input.ResetInputAxes();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+
         if (stage1)
         {
-            Vector3 camPos = new Vector3(playerBases[stage1Index].transform.position.x, playerBases[stage1Index].transform.position.y+2, playerBases[stage1Index].transform.position.z + 5);
+            Vector3 camPos = new Vector3(playerBases[stage1Index].transform.position.x,
+                playerBases[stage1Index].transform.position.y + 2, playerBases[stage1Index].transform.position.z + 5);
             //camera.transform.position = camPos;
-            camera.transform.position = Vector3.Lerp(camera.transform.position, camPos,Time.deltaTime * 2f);
+            camera.transform.position = Vector3.Lerp(camera.transform.position, camPos, Time.deltaTime * 2f);
             camera.transform.LookAt(playerBases[stage1Index].transform.position);
             for (int i = 0; i < players.Count; i++)
             {
@@ -108,6 +114,7 @@ public class PlayerSelect : MonoBehaviour
                 players[i].animator.SetTrigger("Idle");
                 players[i].GetComponent<CapsuleCollider>().enabled = false;
             }
+
             if (show && !showPlayerPicked)
             {
                 text.text = "Player " + playerDisplay + ": " +
@@ -160,19 +167,22 @@ public class PlayerSelect : MonoBehaviour
                 axisCooldownTime = .3f;
                 stage1 = false;
                 stage2 = true;
-                if(stage1Index == 0)
-                currentIndex = stage1Index;
+                if (stage1Index == 0)
+                    currentIndex = stage1Index;
                 if (stage1Index == 1)
                     currentIndex = 5;
                 if (stage1Index == 2)
                     currentIndex = 6;
+                if (stage1Index == 3)
+                    currentIndex = 10;
             }
         }
 
         if (stage2)
         {
             baseIndex = stage1Index;
-            Vector3 camPos = new Vector3(players[currentIndex].transform.position.x, players[currentIndex].transform.position.y + 2, players[currentIndex].transform.position.z + 5);
+            Vector3 camPos = new Vector3(players[currentIndex].transform.position.x,
+                players[currentIndex].transform.position.y + 2, players[currentIndex].transform.position.z + 5);
             //camera.transform.position = camPos;
             camera.transform.position = Vector3.Lerp(camera.transform.position, camPos, Time.deltaTime * 2f);
             camera.transform.LookAt(players[currentIndex].transform.position);
@@ -183,7 +193,7 @@ public class PlayerSelect : MonoBehaviour
                 {
                     if (i == currentIndex)
                     {
-                        players[currentIndex].animator.SetFloat("Forward Amount",0);
+                        players[currentIndex].animator.SetFloat("Forward Amount", 0);
                         players[currentIndex].gameObject.SetActive(true);
                         players[currentIndex].AnimTriggerReset();
                         players[currentIndex].animator.SetTrigger("Idle");
@@ -196,7 +206,8 @@ public class PlayerSelect : MonoBehaviour
                         players[i].animator.SetTrigger("Idle");
                     }
                 }
-                if (Input.GetButtonUp("Fire1") && playerPicking < numPlayers&& axisCooldownTime < 0)
+
+                if (Input.GetButtonUp("Fire1") && playerPicking < numPlayers && axisCooldownTime < 0)
                 {
                     stage1 = true;
                     stage2 = false;
@@ -251,6 +262,7 @@ public class PlayerSelect : MonoBehaviour
                         currentIndex = 0;
                     }
                 }
+
                 if (Input.GetAxis("Horizontal") < -.2f && playerPicking < numPlayers && axisCooldownTime < 0)
                 {
                     show = true;
@@ -307,8 +319,8 @@ public class PlayerSelect : MonoBehaviour
                     if (!picked)
                     {
                         text.text = "Player " + playerDisplay + " You Chose " + players[currentIndex].name +
-                                    "\nPlayer " + (playerDisplay+1) + " Choose Your Character.";
-                        
+                                    "\nPlayer " + (playerDisplay + 1) + " Choose Your Character.";
+
                         show = false;
                         playersPicked.Add(currentIndex);
                         used.Add(currentIndex);
@@ -348,6 +360,7 @@ public class PlayerSelect : MonoBehaviour
                         currentIndex = 5;
                     }
                 }
+
                 if (Input.GetAxis("Horizontal") < -.2f && playerPicking < numPlayers && axisCooldownTime < 0)
                 {
                     show = true;
@@ -445,6 +458,7 @@ public class PlayerSelect : MonoBehaviour
                         currentIndex = 6;
                     }
                 }
+
                 if (Input.GetAxis("Horizontal") < -.2f && playerPicking < numPlayers && axisCooldownTime < 0)
                 {
                     show = true;
@@ -462,6 +476,104 @@ public class PlayerSelect : MonoBehaviour
                     if (currentIndex < 6)
                     {
                         currentIndex = 9;
+                    }
+                }
+            }
+
+            if (baseIndex == 3)
+            {
+                for (int i = 10; i <= 13; i++)
+                {
+                    if (i == currentIndex)
+                    {
+                        players[currentIndex].animator.SetFloat("Forward Amount", 0);
+                        players[currentIndex].gameObject.SetActive(true);
+                        players[currentIndex].AnimTriggerReset();
+                        players[currentIndex].animator.SetTrigger("Idle");
+                    }
+                    else
+                    {
+                        players[i].animator.SetFloat("Forward Amount", 0);
+                        players[i].gameObject.SetActive(false);
+                        players[i].AnimTriggerReset();
+                        players[i].animator.SetTrigger("Idle");
+                    }
+                }
+
+                if (Input.GetButtonUp("Fire1") && playerPicking < numPlayers && axisCooldownTime < 0)
+                {
+                    stage1 = true;
+                    stage2 = false;
+                    for (int i = 0; i < used.Count; i++)
+                    {
+                        if (currentIndex == used[i])
+                        {
+                            picked = true;
+                        }
+                    }
+
+                    if (!picked)
+                    {
+                        text.text = "Player " + playerDisplay + " You Chose " + players[currentIndex].name +
+                                    "\nPlayer " + (playerDisplay + 1) + " Choose Your Character.";
+
+                        show = false;
+                        playersPicked.Add(currentIndex);
+                        used.Add(currentIndex);
+                        playerPicking++;
+                        //players[currentIndex].gameObject.SetActive(false);
+                        players[currentIndex].animator.SetTrigger("Hit");
+                        //currentIndex++;
+                        showPlayerPicked = false;
+                        if (playerPicking >= numPlayers)
+                        {
+                            gameStartTimer = 6f;
+                        }
+                    }
+                    else
+                    {
+                        showPlayerPicked = true;
+                        picked = false;
+                    }
+                }
+
+                if (Input.GetAxis("Horizontal") > .2f && playerPicking < numPlayers && axisCooldownTime < 0)
+                {
+                    show = true;
+                    axisCooldownTime = .33f;
+                    players[currentIndex].AnimTriggerReset();
+                    currentIndex++;
+                    for (int i = 0; i < used.Count; i++)
+                    {
+                        if (currentIndex == used[i])
+                        {
+                            //currentIndex++;
+                        }
+                    }
+
+                    if (currentIndex > 13)
+                    {
+                        currentIndex = 10;
+                    }
+                }
+
+                if (Input.GetAxis("Horizontal") < -.2f && playerPicking < numPlayers && axisCooldownTime < 0)
+                {
+                    show = true;
+                    axisCooldownTime = .33f;
+                    players[currentIndex].AnimTriggerReset();
+                    currentIndex--;
+                    for (int i = 0; i < used.Count; i++)
+                    {
+                        if (currentIndex == used[i])
+                        {
+                            //currentIndex++;
+                        }
+                    }
+
+                    if (currentIndex < 10)
+                    {
+                        currentIndex = 13;
                     }
                 }
             }
---
+++ Assets/Scripts/Spells/FrogMelee.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMelee : Spell
+{
+    
+    public override void ThrowSpell(Vector3 direction, float charge)
+    {
+        if (!affectedByCharge) { charge = 1; }
+        transform.forward = direction;
+
+        Disable(duration);
+    }
+
+    void Update()
+    {
+     
+    }
+    public void EnableCollider()
+    {
+
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (!collisions) { return; }
+        if (explosion) { Destroy(Instantiate(explosion, transform.position, Quaternion.identity), 3f); }
+        if (other.gameObject.CompareTag("Player") && other.gameObject.GetComponent<Player>() != GameManager.instance.CurrentPlayer)
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+            Vector3 playerDirection = player.transform.position - transform.position;
+
+            //soundPlay = GameObject.Find("soundManager");
+            //soundScript sound = soundPlay.GetComponent(typeof(soundScript)) as soundScript;
+            //sound.playIce();
+            player.Damage(contactDamage);
+            player.GetComponent<Rigidbody>().Sleep();
+            playerDirection.Normalize();
+            player.rigidbody.AddForce(playerDirection.x * knockBackForce,
+                    (playerDirection.y / 4f + 1f) * knockBackForce,
+                    playerDirection.z * knockBackForce);
+            
+
+        }
+    }
+}
+
